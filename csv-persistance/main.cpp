44e59e0413b4268cd5aba781fef06d4a69e782c1
vhristov <valericfbg@gmail.com>
2020-02-07 16:43:24 +0200

    initial commit

--- csv-persistance/main.cpp
+++ csv-persistance/main.cpp
@@ -1,6 +1,12 @@
 //
 //  main.cpp
 //  csv-persistance
+// Features:
+// - Should support multiple DBs and tables.
+// - Can list DBs and when DB is selected it should list tables.
+// - Should support SQL like syntax for querying the DB. (insert, select, update, delete)
+// - Should support joins with two tables.
+// - Should have some validation on insert and update.
 //
 //  Created by Valeri Hristov on 7.02.20.
 //  Copyright © 2020 Valeri Hristov. All rights reserved.
