939befea3070d20232e7b8585e86db9cc588a125
vhristov <valericfbg@gmail.com>
2020-02-07 16:34:12 +0200

    Initial Commit

---
+++ csv-persistance/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  csv-persistance
+//
+//  Created by Valeri Hristov on 7.02.20.
+//  Copyright © 2020 Valeri Hristov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
